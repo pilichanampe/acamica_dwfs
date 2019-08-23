649e87e8a149517c755417662499ba32ed98d65a
pilichanampe <mariadelpilarchanampe@gmail.com>
2019-08-23 14:32:57 -0300

    update metodos Jugador y funcionalidad en Juego

--- TP-05-CZOMBIE/js/juego.js
+++ TP-05-CZOMBIE/js/juego.js
@@ -132,7 +132,7 @@ Juego.capturarMovimiento = function(tecla) {
   if (this.chequearColisiones(movX + this.jugador.x, movY + this.jugador.y)) {
     /* Aca tiene que estar la logica para mover al jugador invocando alguno
     de sus metodos  */
-      Jugador.moverse();
+      Jugador.mover(movX, movY);
     /* COMPLETAR */
   }
 };
--- TP-05-CZOMBIE/js/jugador.js
+++ TP-05-CZOMBIE/js/jugador.js
@@ -10,21 +10,53 @@ var Jugador = {
   alto: 30,
   velocidad: 10,
   vidas: 5,
+
+  /*mover(x,y){
+    if(x > 0){
+      this.sprite = "imagenes/auto_rojo_derecha.png";
+      this.ancho = 30;
+      this.alto = 15;
+    } else if(x < 0) {
+      this.sprite = "imagenes/auto_rojo_izquierda.png";
+      this.ancho = 30;
+      this.alto = 15;
+    } else if(y > 0){
+      this.sprite = "imagenes/auto_rojo_abajo.png";
+      this.ancho = 15;
+      this.alto = 30;
+    } else if(y < 0){
+      this.sprite = "imagenes/auto_rojo_arriba.png";
+      this.ancho = 15;
+      this.alto = 30;
+    }
+  this.x += x;
+  this.y += y;
+  }*/
     // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
   // y todo lo que haga falta para que cumpla con sus responsabilidades
 }
 
-Jugador.moverse = function() {
-  switch () {
+Jugador.prototype.moverse = function() {
+  switch (true) {
     case 'der':
+      Jugador.prototype.setSprite = function(){
+        this.sprite = 'imagenes/auto_rojo_derecha.png';
+        this.ancho = 30;
+        this.alto = 15;
+        break;
+      }
       this.x += 5;
       break;
 
     case 'izq':
       this.x -= 5;
+      break;
     case 'arr':
       this.y += 5;
+      break;
     case 'abajo':
       this.y -= 5;
+      break;
+    default:
   }
 }

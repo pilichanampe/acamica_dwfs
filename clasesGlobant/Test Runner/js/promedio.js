6d0318fca4b7b5aa8fa3f270648eac14337491fb
pilichanampe <pilichanampe@hotmail.com>
2019-08-29 20:47:20 -0300

    descargadas presentaciones y test runner zip, testing de la clase del 27/08

---
+++ clasesGlobant/Test Runner/js/promedio.js
@@ -0,0 +1,23 @@
+function sumatoria(elementos) {
+    var suma = 0;
+    for(var i = 0; i < elementos.length; i++) {
+      if (typeof elementos[i] === 'number') {
+        suma += elementos[i];
+      }	
+    }
+  
+    return suma;
+  }
+  
+  function division(a, b) {
+    return a/b;
+  }
+
+  function promedio(elementos) {
+    var suma = sumatoria(elementos);
+    var promedio = division(suma, elementos.length);
+  
+    return promedio;
+  }
+
+  

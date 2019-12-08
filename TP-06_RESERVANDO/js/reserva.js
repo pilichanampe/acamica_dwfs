2b69162b996888d694552d1ce272ade8654ecaae
pilichanampe <mariadelpilarchanampe@gmail.com>
2019-12-08 12:21:19 -0300

    reseva.js file created

---
+++ TP-06_RESERVANDO/js/reserva.js
@@ -0,0 +1,6 @@
+var Reserva = function(fechaHora, cantPersonas, precioPersona, descuento) {
+    this.fechaHora = fechaHora;
+    this.cantPersonas = cantPersonas;
+    this.precioPersona = precioPersona;
+    this.descuento = descuento;
+}

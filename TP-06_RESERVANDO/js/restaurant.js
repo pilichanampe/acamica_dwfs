2443d09dc601fcb7040fe7bee4dcd656af4993fe
pilichanampe <mariadelpilarchanampe@gmail.com>
2019-12-08 00:10:56 -0300

    guia 2, refactoryng function obtenerPuntuacion(), creating function sumatoria() and function promedio()

--- TP-06_RESERVANDO/js/restaurant.js
+++ TP-06_RESERVANDO/js/restaurant.js
@@ -22,13 +22,20 @@ Restaurant.prototype.obtenerPuntuacion = function() {
     if (this.calificaciones.length === 0) {
         return 0;
     } else {
-        var sumatoria = 0;
-        for (var i = 0; i < this.calificaciones.length; i++) {
-            sumatoria += this.calificaciones[i]
-        }
-        var promedio = sumatoria / this.calificaciones.length;
-        return Math.round(promedio * 10) / 10;
+        return this.promedio(this.calificaciones);
     }
 
+}
+
+Restaurant.prototype.sumatoria = function(arregloASumar){
+    let sumatoria = 0;
+
+    arregloASumar.forEach(element => sumatoria += element);
+
+    return sumatoria;
+}
+
+Restaurant.prototype.promedio = function(arregloAPromediar) {
+    return Math.round(this.sumatoria(arregloAPromediar) / arregloAPromediar.length * 10) / 10;
 }
 

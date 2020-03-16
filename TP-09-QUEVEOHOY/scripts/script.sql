b4027911e1642349a955b01d1e0504ffce6ad6e1
pilichanampe <mariadelpilarchanampe@gmail.com>
2020-03-16 12:08:03 -0300

    creating table pelicula

--- TP-09-QUEVEOHOY/scripts/script.sql
+++ TP-09-QUEVEOHOY/scripts/script.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE que_veo_hoy;
+CREATE TABLE pelicula (
+    id INT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    duracion INT(5),
+    director VARCHAR(400),
+    anio INT(5),
+    fecha_lanzamiento DATE,
+    puntuacion INT(2),
+    poster VARCHAR(300),
+    trama VARCHAR(700),
+    PRIMARY KEY (id)
+);

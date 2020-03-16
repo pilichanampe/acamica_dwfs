a114cedec0d96ee7ffc860b0c06821202a9798eb
pilichanampe <mariadelpilarchanampe@gmail.com>
2020-03-16 12:08:52 -0300

    require controladorPeliculas

--- TP-09-QUEVEOHOY/servidor/servidor.js
+++ TP-09-QUEVEOHOY/servidor/servidor.js
@@ -3,6 +3,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+var controladorPeliculas = require ('./controladores/controladorPeliculas');
+
+
 var app = express();
 
 app.use(cors());

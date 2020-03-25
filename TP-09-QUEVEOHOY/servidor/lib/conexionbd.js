5f6363abb1800b8497fb3981d51ac54100382a9c
pilichanampe <mariadelpilarchanampe@gmail.com>
2020-03-25 16:38:20 -0300

    creating dependences

--- TP-09-QUEVEOHOY/servidor/lib/conexionbd.js
+++ TP-09-QUEVEOHOY/servidor/lib/conexionbd.js
@@ -1,11 +1,11 @@
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 
-  port     : 
-  user     : 
-  password : 
-  database : 
+  host     : 'localhost',
+  port     : '3306',
+  user     : 'root',
+  password : '129022513',
+  database : 'que_veo_hoy'
 });
 
 module.exports = connection;
